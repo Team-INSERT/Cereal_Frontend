46cb148c8c6e5555f42c6b8e59aa8c4713dd3607
Ubinquitous <ubinquitous1@gmail.com>
2023-01-29 17:24:20 +0900

    ADD : ChatBox

---
+++ src/components/ChatBox/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import * as S from './style'
+
+const ChatBox = () => {
+	return (
+		<S.ChatBoxWrap>
+			<S.ChatBoxWarnWrap>
+				<S.ChatBoxTitle>이런 점을 주의해주세요!</S.ChatBoxTitle>
+				<S.ChatBoxDescription>
+					대화할 때에, 통신매체이용음란죄 등의 법에
+					<br /> 저촉되는 내용이나, 누군가를 비난하고 모욕
+					<br />
+					하는 등 부적절한 내용의 대화는 피해주세요.
+					<br />
+					<br />말 못할 고민이나, 하소연 등을 풀어보세요.
+					<br /> 올바른 채팅 에티켓을 지키며 채팅합시다!
+				</S.ChatBoxDescription>
+			</S.ChatBoxWarnWrap>
+			<S.ChatBoxLine />
+			<S.ChatRecommendWrap>
+				<S.ChatBoxTitle>이런 질문을 해봐요!</S.ChatBoxTitle>
+				<img src="assets/test.png" width={260} />
+			</S.ChatRecommendWrap>
+		</S.ChatBoxWrap>
+	)
+}
+
+export default ChatBox
---
+++ src/components/ChatBox/style.ts
@@ -0,0 +1,44 @@
+import styled from 'styled-components'
+
+export const ChatBoxWrap = styled.div`
+	margin-left: auto;
+	width: 360px;
+	height: 100%;
+	box-shadow: -4px 0px 5px -2px #ebebeb;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`
+
+export const ChatBoxWarnWrap = styled.div`
+	height: 40%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`
+
+export const ChatBoxTitle = styled.span`
+	font-size: 22px;
+	font-weight: 800;
+	margin: 30px 0 20px 0;
+	color: #007aff;
+`
+
+export const ChatBoxDescription = styled.span`
+	font-size: 15px;
+	font-weight: 400;
+	text-align: left;
+`
+
+export const ChatBoxLine = styled.div`
+	width: 80%;
+	height: 2px;
+	background-color: #ccc;
+`
+
+export const ChatRecommendWrap = styled.div`
+	height: 60%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`
