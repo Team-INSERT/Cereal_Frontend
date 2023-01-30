335fbe81bf9bd02005af0bd6d5f10b828ab30826
Ubinquitous <ubinquitous1@gmail.com>
2023-01-30 12:23:46 +0900

    ADD : Components flex 지정

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -6,6 +6,13 @@ const Chat = () => {
 	return (
 		<S.ChatWrap>
 			<S.ChatBackboardWrap>
+				<S.ChattingWrap>
+					<S.ChattingHeader>
+						<S.ChattingHeaderText>익명님과의 대화</S.ChattingHeaderText>
+					</S.ChattingHeader>
+					<S.ChattingBox></S.ChattingBox>
+					<S.ChattingSend></S.ChattingSend>
+				</S.ChattingWrap>
 				<ChatBox />
 			</S.ChatBackboardWrap>
 		</S.ChatWrap>
--- src/pages/Chat/style.ts
+++ src/pages/Chat/style.ts
@@ -17,5 +17,30 @@ export const ChatBackboardWrap = styled.div`
 	display: flex;
 	justify-content: center;
 	align-items: center;
-	flex-direction: column;
+`
+export const ChattingWrap = styled.div`
+	width: 100%;
+	height: 100%;
+`
+
+export const ChattingHeader = styled.div`
+	width: 100%;
+	height: 8%;
+	background-color: pink;
+`
+
+export const ChattingBox = styled.div`
+	width: 100%;
+	height: 82%;
+	background-color: green;
+`
+
+export const ChattingSend = styled.div`
+	width: 100%;
+	height: 10%;
+	background-color: blue;
+`
+
+export const ChattingHeaderText = styled.span`
+	font-size: 16px;
 `
