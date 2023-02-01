2aed84d0546e145bda3ce7a9af409c876428ac21
Ubinquitous <ubinquitous1@gmail.com>
2023-02-01 15:30:41 +0900

    UPDATE : 채팅 UI 설계 완료

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -8,12 +8,36 @@ const Chat = () => {
 			<S.ChatBackboardWrap>
 				<S.ChattingWrap>
 					<S.ChattingHeader>
+						<S.ChattingProfileImage src="https://bssm.kro.kr/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fprofile_default.99e93808.png&w=128&q=75" />
 						<S.ChattingHeaderText>익명님과의 대화</S.ChattingHeaderText>
 					</S.ChattingHeader>
-					<S.ChattingBox></S.ChattingBox>
-					<S.ChattingSend>
-						<img src="" />
-					</S.ChattingSend>
+					<S.ChattingLine />
+					<S.ChattingBox>
+						<S.ChatBox>
+							<S.MyChatDate>오후 2:48</S.MyChatDate>
+							<S.MyChat>TEST TEST</S.MyChat>
+						</S.ChatBox>
+						<S.ChatBox>
+							<S.MyChatDate>오후 2:48</S.MyChatDate>
+							<S.MyChat>
+								TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
+							</S.MyChat>
+						</S.ChatBox>
+						<S.ChatBox>
+							<S.YourChat>TEST TEST TEST TEST TEST TEST TEST </S.YourChat>
+							<S.YourChatDate>오후 2:49</S.YourChatDate>
+						</S.ChatBox>
+						{[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((index) => (
+							<S.ChatBox key={index}>
+								<S.YourChat>TEST</S.YourChat>
+								<S.YourChatDate>오후 2:49</S.YourChatDate>
+							</S.ChatBox>
+						))}
+					</S.ChattingBox>
+					<S.ChattingSendBox>
+						<S.ChattingSend />
+						<S.ChattingSendButton>전송</S.ChattingSendButton>
+					</S.ChattingSendBox>
 				</S.ChattingWrap>
 				<ChatBox />
 			</S.ChatBackboardWrap>
--- src/pages/Chat/style.ts
+++ src/pages/Chat/style.ts
@@ -21,26 +21,118 @@ export const ChatBackboardWrap = styled.div`
 export const ChattingWrap = styled.div`
 	width: 100%;
 	height: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
 `
 
 export const ChattingHeader = styled.div`
 	width: 100%;
 	height: 8%;
-	background-color: pink;
+	display: flex;
+	align-items: center;
 `
 
 export const ChattingBox = styled.div`
 	width: 100%;
-	height: 82%;
-	background-color: green;
+	height: 78%;
+	overflow: scroll;
 `
 
-export const ChattingSend = styled.div`
+export const ChattingSendBox = styled.form`
+	margin-top: 10px;
 	width: 100%;
-	height: 10%;
-	background-color: blue;
+	height: 14%;
+	box-shadow: 0px -6px 5px -4px #ebebeb;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+`
+
+export const ChattingSend = styled.textarea`
+	width: 96%;
+	height: 80%;
+	resize: none;
+	border: none;
+	outline: none;
+	font-size: 14px;
+	padding: 10px 18px 0 18px;
+`
+
+export const ChattingSendButton = styled.button`
+	margin: 0 10px 8px auto;
+	width: 60px;
+	height: 48px;
+	border: none;
+	background-color: #ccc;
+	border-radius: 4px;
+	color: white;
+	font-weight: 600;
 `
 
 export const ChattingHeaderText = styled.span`
 	font-size: 16px;
+	font-weight: 700;
+	margin: 2px 0 0 12px;
+`
+
+export const ChattingProfileImage = styled.img`
+	width: 30px;
+	height: 30px;
+	border-radius: 999px;
+	border: 1px solid #ccc;
+	margin-left: 12px;
+`
+
+export const ChattingLine = styled.div`
+	width: 100%;
+	height: 2px;
+	margin-bottom: 6px;
+	background-color: #ccc;
+`
+
+export const ChatBox = styled.div`
+	width: 100%;
+	height: fit-content;
+	display: flex;
+`
+
+export const MyChat = styled.div`
+	width: fit-content;
+	height: fit-content;
+	max-width: 400px;
+	background-color: #007aff;
+	border-radius: 6px;
+	margin: 4px 16px 4px 0;
+	padding: 8px 12px 8px 12px;
+	text-align: left;
+	color: white;
+	font-weight: 560;
+	font-size: 14px;
+`
+
+export const YourChat = styled.div`
+	width: fit-content;
+	height: fit-content;
+	max-width: 400px;
+	background-color: #aaa;
+	border-radius: 6px;
+	margin: 4px 6px 4px 12px;
+	padding: 8px 12px 8px 12px;
+	text-align: left;
+	color: white;
+	font-weight: 560;
+	font-size: 14px;
+`
+
+export const MyChatDate = styled.span`
+	margin: auto 4px 4px auto;
+	color: #ccc;
+	font-size: 12px;
+`
+
+export const YourChatDate = styled.span`
+	margin: auto 4px 4px 0;
+	color: #ccc;
+	font-size: 12px;
 `
