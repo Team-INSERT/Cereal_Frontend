e482b4eedb4d90d7f349018e850b50434dd60b57
Ubinquitous <ubinquitous1@gmail.com>
2023-02-03 10:02:03 +0900

    ADD : dateParser

---
+++ src/utils/dateParset.ts
@@ -0,0 +1,10 @@
+const dateParser = (date: string) => {
+	if (parseInt(date.substring(11, 13)) > 12) {
+		const date12 = parseInt(date.substring(11, 13)) - 12
+		return `오후 ${date12}${date.substring(14, 16)}`
+	} else {
+		return `오전 ${date.substring(11, 16)}`
+	}
+}
+
+export default dateParser
