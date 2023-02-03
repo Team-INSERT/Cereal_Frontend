fed3ba8b42e3df789a8a0ac1d5604f8f677077c0
Ubinquitous <ubinquitous1@gmail.com>
2023-02-03 10:01:26 +0900

    FIX : 문자수정

--- src/components/ChatBox/index.tsx
+++ src/components/ChatBox/index.tsx
@@ -13,21 +13,15 @@ const ChatBox = () => {
 		<S.ChatBoxWrap>
 			<S.ChatBoxWarnWrap>
 				<S.ChatBoxTitle>이런 점을 주의해주세요!</S.ChatBoxTitle>
-				{/*<S.ChatBoxDescription>*/}
-				{/*	대화할 때에, 통신매체이용음란죄 등의 법에*/}
-				{/*	<br /> 저촉되는 내용이나, 누군가를 비난하고 모욕*/}
-				{/*	<br />*/}
-				{/*	하는 등 부적절한 내용의 대화는 피해주세요.*/}
-				{/*	<br />*/}
-				{/*	<br />말 못할 고민이나, 하소연 등을 풀어보세요.*/}
-				{/*	<br /> 올바른 채팅 에티켓을 지키며 채팅합시다!*/}
-				{/*</S.ChatBoxDescription>*/}
 				<S.ChatBoxDescription>
-					니 좆대로 하세요
-					<br/>
-					근데 고소 들어오면 넌 좆된거에요 ^^
+					대화할 때에, 통신매체이용음란죄 등의 법에
+					<br /> 저촉되는 내용이나, 누군가를 비난하고 모욕
+					<br />
+					하는 등 부적절한 내용의 대화는 피해주세요.
+					<br />
+					<br />말 못할 고민이나, 하소연 등을 풀어보세요.
+					<br /> 올바른 채팅 에티켓을 지키며 채팅합시다!
 				</S.ChatBoxDescription>
-
 			</S.ChatBoxWarnWrap>
 			<S.ChatBoxLine />
 			<S.ChatRecommendWrap>
